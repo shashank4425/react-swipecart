6d2923fa4208e32cb2c555a3a39bb65693b4f9de
Shashank Singh <singhshashank081@gmail.com>
2021-07-07 18:25:23 +0530

    added images in nevigation link

--- src/components/Nevigationstrip.jsx
+++ src/components/Nevigationstrip.jsx
@@ -5,10 +5,10 @@ export default class Nevigationstrip extends React.Component{
       return <>
            <div className="product_nav">
             <ul className="nav-links">
-             <NavLink exact to="/smartphones">Smartphones</NavLink>
-             <NavLink exact to="/laptops">Laptops</NavLink>
-             <NavLink exact to="/headphones">Headphones</NavLink>
-             <NavLink exact to="/trainingpage">CounterFunction</NavLink>
+            <div className="EC_SM"><NavLink exact to="/smartphones"><div className="img_Sec"><img src="/images/22ff11CC.png"/></div><div className="title">Smartphones</div></NavLink></div>
+            <div className="EC_SM"> <NavLink exact to="/laptops"><div className="img_Sec"><img src="/images/laptop3fzhzkvchccq.jpeg"/></div><div className="title">Laptops</div></NavLink></div>
+            <div className="EC_SM"><NavLink exact to="/headphones"><div className="img_Sec"><img src="/images/rockerz-450-boat-original-headp.jpeg"/></div><div className="title">Headphones</div></NavLink></div>
+            <div className="EC_SM"><NavLink exact to="/trainingpage"><div className="img_Sec"></div><div className="title">CounterFunction</div></NavLink></div>
            </ul>
          </div>
       </>
